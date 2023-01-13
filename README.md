83a2319f6c19ac825f9e5d8f3d784c37b172b32f
victorgarciaesgi <victorgarciaparis13@gmail.com>
2023-01-13 14:44:32 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nuxt-typed-router-demo
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-7e4xvw)
---
+++ store/testRouter.ts
@@ -0,0 +1,24 @@
+import { useTypedRouter, useTypedRoute } from '@typed-router';
+
+const router = useTypedRouter();
+
+// @ts-expect-error
+router.push({ name: 'user-id' }); // Error
+
+// @ts-expect-error
+router.push({ name: 'user-id', params: { foo: 'bar' } }); // Error
+
+router.push({ name: 'user-id', params: { id: 1 } }); // Good
+
+const route = useTypedRoute('user-id');
+
+// @ts-expect-error
+route.params.foo;
+
+route.params.id;
+
+const route2 = useTypedRoute();
+
+if (route2.name === 'user-id-posts') {
+  console.log(route2.params); // Try autocomplete here
+}
