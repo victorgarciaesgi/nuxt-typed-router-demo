0a5c8054bdb0d6c3a704d19089d0bddfd571ec14
victorgarciaesgi <victorgarciaparis13@gmail.com>
2023-01-27 18:32:51 +0100

    3 files modified

--- store/testRouter.ts
+++ store/testRouter.ts
@@ -9,9 +9,9 @@ const router = useRouter();
 
 navigateTo({ name: 'user-foo-bar', params: { foo: 'bar' } });
 
-router.push({ name: 'user-id' }); // Error
+router.push({ name: 'user-id' }); // Error ❌
 
-router.push({ name: 'user-id', params: { foo: 'bar' } }); // Error
+router.push({ name: 'user-id', params: { foo: 'bar' } }); // Error ❌
 
 router.push({ name: 'user-id', params: { id: 1 } });
 router.push({ name: 'user-foo-bar', params: { foo: 1 } });
@@ -20,7 +20,7 @@ router.push({ name: 'user-id-slug', params: { slug: [1, 2, 3] } });
 
 const route = useRoute('user-foo-bar');
 
-route.params.id; // Error
+route.params.id; // Error ❌
 
 route.params.bar;
 
