c95e3bc6cf8ae6272ff04021bca5eafb128a67e4
Victor Garcia <victorgarciaparis13@gmail.com>
2023-02-20 09:46:04 +0000

    Update 3 files

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,14 @@
 <template>
   <button @click="navigate">Navigate to user</button>
+  <nuxt-link> Navigate </nuxt-link>
 </template>
 
 <script setup lang="ts">
-import { useRouter, useRoute } from '@typed-router';
+import { useRouter, useRoute } from "@typed-router";
 
 const router = useRouter();
 
 function navigate() {
-  router.push({ name: 'user-id', params: { id: 1 } });
+  router.push({ name: "user-id", params: { id: 1 } });
 }
 </script>
